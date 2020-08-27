b9e10fc99749809878368581dc50492a589948e3
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-27 11:50:56 -0500

    I accidently had two variable names set to input number two, I changed this so that there is now a variable set to input three. all tests for this script should pass

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -3,8 +3,8 @@
 ####  CONSTANTS #####
 FILE_NAME=$1
 SPECIFIER=$2
-RESULTING_FILE_NAME=$2
+RESULTING_FILE_NAME=$3
 ##### MAIN #####
 
-cat "$SPECIFIER"_header.html  "$FILE_NAME" "$SPECIFIER"_footer.html |> "$RESULTING_FILE_NAME"
+cat "$SPECIFIER"_header.html  "$FILE_NAME" "$SPECIFIER"_footer.html >> "$RESULTING_FILE_NAME"
 
