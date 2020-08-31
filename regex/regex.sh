68ba874feae5ad7faaf2e790a8c48d313b35dcf6
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-31 10:51:47 -0500

    I finished the regex.sh file. after struglling with sed I went back to awk and used NF-1 to solve the problem I was having with rempving the period at the end of the string

--- regex/regex.sh
+++ regex/regex.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-# s specifies substitution operation
-# using the /1 and /2 flags to replace the respective occurance of a pattern in a line 
+# using sed to create the first output file, sed code was provided
+# tee allows for the output to be displayed and saved into a file
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt | tee r0_output.txt
 
- 
-awk 'NR == 1 || NR == 2|| NR == 4{print "1. " $4 "\n" "2. " $9}' r1_input.txt > r1_output.txt 
+# r1_input.txt -F flag is used here for flag it makes it so the input display correctly
+# this takes advantage of the fact that all of the wanted text includes "My favorite sandwhich"
+# NF is the number of fields in the current record, so the NF-1 here refers to the second from last value
+# \n refers to new line
+awk -F  "[. ]+" '/My favorite sandwich is /{print  "1. " $4, "\n""2. "$(NF-1), "\n"}' r1_input.txt > r1_output.txt
+
+#r2_input.txt using -E to make matching possible in sed and the s to mean substution
+# here we are substuting the 'sandwhich with' line 
+# \n signinfies a new line
+#(\S+) lets us get the words delineated my periods becasue they do not contain "white spaces"
 sed -E 's/\* sandwich with (\S+) (for here|to go)/1. \1\n2. \2\n/' r2_input.txt > r2_output.txt
