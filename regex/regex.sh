5be37a04e09281dfce1b3583a28242443fc5e6d8
Cassie Schultz <schu4276@morris.umn.edu>
2020-08-31 09:31:54 -0500

    I have gotten the tests to past for the first and last task by using the sed -E.  I found it difficult to use sed for the r1_input so I have attempted using awk.  I will adjust the awk command to try and use the matching. I tried using $n to just skip over the matching and take a shortcut by just printing out input but I have run into a snag with the period at the end of the sentences

--- regex/regex.sh
+++ regex/regex.sh
@@ -2,4 +2,7 @@
 # s specifies substitution operation
 # using the /1 and /2 flags to replace the respective occurance of a pattern in a line 
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt | tee r0_output.txt
-awk 'match($0, /([A-Z]), (
+
+ 
+awk 'NR == 1 || NR == 2|| NR == 4{print "1. " $4 "\n" "2. " $9}' r1_input.txt > r1_output.txt 
+sed -E 's/\* sandwich with (\S+) (for here|to go)/1. \1\n2. \2\n/' r2_input.txt > r2_output.txt
